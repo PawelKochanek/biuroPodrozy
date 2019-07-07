9b789569e55463067572a736ee2850f7a6dde59b
PawelKochanek <pawelkochanek@gazeta.pl>
2019-07-07 11:24:36 +0200

    wyswietlenie listy wycieczek

--- src/main/java/pl/piotrpawel/biuropodrozy/Trip.java
+++ src/main/java/pl/piotrpawel/biuropodrozy/Trip.java
@@ -1,12 +1,10 @@
 package pl.piotrpawel.biuropodrozy;
 
-import lombok.Getter;
-import lombok.Setter;
+
 import pl.piotrpawel.biuropodrozy.config.TripType;
 
 import java.time.LocalDate;
 
-@Getter @Setter
 public class Trip {
 
     String travelFrom; // miasto, lotnisko
@@ -38,5 +36,110 @@ public class Trip {
         this.isPromoted = isPromoted;
         this.howManyAdultsAreTraveling = howManyAdultsAreTraveling;
         this.howManyChildrenAreTraveling = howManyChildrenAreTraveling;
+    }
+
+    public String getTravelFrom() {
+        return travelFrom;
+    }
+
+    public void setTravelFrom(String travelFrom) {
+        this.travelFrom = travelFrom;
+    }
+
+    public String getTravelTo() {
+        return travelTo;
+    }
+
+    public void setTravelTo(String travelTo) {
+        this.travelTo = travelTo;
+    }
+
+    public LocalDate getDateTravelFrom() {
+        return dateTravelFrom;
+    }
+
+    public void setDateTravelFrom(LocalDate dateTravelFrom) {
+        this.dateTravelFrom = dateTravelFrom;
+    }
+
+    public LocalDate getDateTravelTo() {
+        return dateTravelTo;
+    }
+
+    public void setDateTravelTo(LocalDate dateTravelTo) {
+        this.dateTravelTo = dateTravelTo;
+    }
+
+    public int getDaysNumber() {
+        return daysNumber;
+    }
+
+    public void setDaysNumber(int daysNumber) {
+        this.daysNumber = daysNumber;
+    }
+
+    public TripType getTripType() {
+        return tripType;
+    }
+
+    public void setTripType(TripType tripType) {
+        this.tripType = tripType;
+    }
+
+    public long getPriceForAdult() {
+        return priceForAdult;
+    }
+
+    public void setPriceForAdult(long priceForAdult) {
+        this.priceForAdult = priceForAdult;
+    }
+
+    public long getPriceForChild() {
+        return priceForChild;
+    }
+
+    public void setPriceForChild(long priceForChild) {
+        this.priceForChild = priceForChild;
+    }
+
+    public boolean isPromoted() {
+        return isPromoted;
+    }
+
+    public void setPromoted(boolean promoted) {
+        isPromoted = promoted;
+    }
+
+    public int getHowManyAdultsAreTraveling() {
+        return howManyAdultsAreTraveling;
+    }
+
+    public void setHowManyAdultsAreTraveling(int howManyAdultsAreTraveling) {
+        this.howManyAdultsAreTraveling = howManyAdultsAreTraveling;
+    }
+
+    public int getHowManyChildrenAreTraveling() {
+        return howManyChildrenAreTraveling;
+    }
+
+    public void setHowManyChildrenAreTraveling(int howManyChildrenAreTraveling) {
+        this.howManyChildrenAreTraveling = howManyChildrenAreTraveling;
+    }
+
+    @Override
+    public String toString() {
+        return "Trip{" +
+                "travelFrom='" + travelFrom + '\'' +
+                ", travelTo='" + travelTo + '\'' +
+                ", dateTravelFrom=" + dateTravelFrom +
+                ", dateTravelTo=" + dateTravelTo +
+                ", daysNumber=" + daysNumber +
+                ", tripType=" + tripType +
+                ", priceForAdult=" + priceForAdult +
+                ", priceForChild=" + priceForChild +
+                ", isPromoted=" + isPromoted +
+                ", howManyAdultsAreTraveling=" + howManyAdultsAreTraveling +
+                ", howManyChildrenAreTraveling=" + howManyChildrenAreTraveling +
+                '}';
     }
 }
--- src/main/java/pl/piotrpawel/biuropodrozy/User.java
+++ src/main/java/pl/piotrpawel/biuropodrozy/User.java
@@ -1,4 +1,28 @@
 package pl.piotrpawel.biuropodrozy;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
 public class User {
+    private String name;
+    private String lastname;
+    private String email;
+    private String country;
+    private String city;
+    private String adress;
+    private int phoneNumber;
+
+    public User() {}
+
+    public User(String name, String lastname, String email,
+                String country, String city, String adress, int phoneNumber) {
+        this.name = name;
+        this.lastname = lastname;
+        this.email = email;
+        this.country = country;
+        this.city = city;
+        this.adress = adress;
+        this.phoneNumber = phoneNumber;
+    }
 }
